8bf6dfa8ee6133b72cd31c38e2a085535e990201
Lucas Berthet <lu.berthet@gmail.com>
2026-03-05 23:48:11 +0100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://sharing-links-transformer.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://sharing-links-transformer.vercel.app'],
   });
   const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0');
